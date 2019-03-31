8de7cb9ed64ca6b7f6b34b82be4b31f6ca75dfd9
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-03-31 12:31:20 -0400

    ENH: Add Python `setup.py` file
    Add Python `setup.py` file.

---
+++ setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from os import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('scikit-build is required to build from source.', file=sys.stderr)
+    print('Please run:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  python -m pip install scikit-build')
+    sys.exit(1)
+
+setup(
+    name='itk-ioscanco',
+    version='0.0.1',
+    author='David Gobbi',
+    author_email='david.gobbi@gmail.com',
+    packages=['itk'],
+    package_dir={'itk': 'itk'},
+    download_url=r'https://github.com/KitwareMedical/ITKIOScanco',
+    description=r'An ITK module to read and write Scanco microCT .isq and .aim files',
+    long_description='ITKIOScanco provides classes to read and write Scanco '
+                     'to read and write Scanco microCT .isq and .aim files. ',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: Android",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS"
+        ],
+    license='Apache',
+    keywords='ITK InsightToolkit scanco imaging micro-ct',
+    url=r'https://github.com/KitwareMedical/ITKIOScanco',
+    install_requires=[
+        r'itk>=5.0b01'
+    ]
+    )
