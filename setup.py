c02636bbc1e9afa48649f63cad895ae12059be61
Matt McCormick <matt.mccormick@kitware.com>
2021-04-06 13:28:07 -0400

    ENH: Bump Python package version to 0.9.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-ioscanco',
-    version='0.9.0',
+    version='0.9.1',
     author='Matt McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
